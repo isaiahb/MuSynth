ec456c088e5f7475fa6d614571b61f328f0c4610
isaiah <isaiahballah@gmail.com>
2019-12-02 16:29:13 -0500

    renamed static folder to public

--- backend/lib/server.js
+++ backend/lib/server.js
@@ -2,23 +2,20 @@ const express = require("express");
 var app = express();
 var http = require('http').createServer(app);
 app.use(require("morgan")("dev"));
-app.use(express.static('./fullFrontEnd'));
+app.use(express.static('./public'));
 
 var io = require('socket.io')(http, { 'transports': ['websocket', 'polling'] });
-
 
 var spawn = require('child_process').spawn;
 var py = spawn('python3', ['dsp.py']);
 
 py.stdout.on('data', function (data) {
-	// dataString += data.toString();
 	console.log(data);
 });
 
 py.on("error", (err)=>console.log(err));
 
 var buffer = [];
-
 function add(data) {
 	if (buffer.length >= 256 * 10) {
 		buffer = data.concat(buffer.slice(0, buffer.length - data.length));
@@ -27,12 +24,13 @@ function add(data) {
 	}
 }
 
+
 io.on('connection', function (socket) {
 	console.log("new connection")
 
 	//receive processed data from python send to audience clients
 	socket.on("python", function (data) {
-		console.log("python " + data);
+		// console.log("python " + data);
 		io.emit('notes', data);
 
 	});
@@ -41,7 +39,11 @@ io.on('connection', function (socket) {
 	socket.on("floats", function (data) {
 		add(data);
 		io.emit('process', buffer);
-		// console.log("buffer lenngth:  " + buffer.length);
+	});
+
+	//receive data from muscician client2
+	socket.on("int", function (data) {
+		io.emit('notes', data);
 	});
 
 });
